4706fa51ce845ae6e11332e48669efa6ffb2652f
Fraciman <115511589+Fraciman@users.noreply.github.com>
2025-11-11 00:31:42 +0800

    Add files via upload

---
+++ love-site/js/config.js
@@ -0,0 +1,12 @@
+
+// Edit this file with your details and photo filenames in /assets/img
+window.SITE = {
+  name: "SUHA",
+  from: "IMAN",
+  message: "In a world full of ordinary days, you are my favorite moment. Happy birthday, sayang.",
+  photos: [
+    // Put your image filenames here (they must exist under /assets/img)
+    // Example images (replace with your own):
+    1,
+  ]
+};
